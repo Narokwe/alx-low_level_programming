0c049d89aff89d093a8e2825ce1b88a74d74714e
Narokwe <narokweaugustine@gmail.com>
2023-07-13 07:04:30 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <errno.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
