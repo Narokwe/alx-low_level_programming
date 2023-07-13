c20430102962e144345274bf367648b2a17c30c5
Narokwe <narokweaugustine@gmail.com>
2023-07-13 06:43:25 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void) {
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	if (write(STDERR_FILENO, message, strlen(message)) == -1) {
+		write(STDERR_FILENO, strerror(errno), strlen(strerror(errno)));
+	}
+
+	return 1;
+}
